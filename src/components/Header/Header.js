6700e8ade2428eff61a237f1ea224397ced76dcd
Angel-Journey <angel.journey7@gmail.com>
2022-03-07 23:47:49 -0600

    Add logo images and update NavBar styling
    NavBar Brand no longer has a link
    NavBar Brand is an image and brand name
    NavBar options turn gold when hovered over
    Overall NavBar spacing has been adjusted
    Default page is now Events

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,32 +1,38 @@
 import React, { Fragment } from 'react'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
-const authenticatedOptions = (
-  <Fragment>
-    <NavLink to='/change-password' className='nav-link'>Change Password</NavLink>
-    <NavLink to='/sign-out' className='nav-link'>Sign Out</NavLink>
-  </Fragment>
-)
-
-const unauthenticatedOptions = (
-  <Fragment>
-    <NavLink to='/sign-up' className='nav-link'>Sign Up</NavLink>
-    <NavLink to='/sign-in' className='nav-link'>Sign In</NavLink>
-  </Fragment>
-)
+// const authenticatedOptions = (
+//   <Fragment>
+//     <NavLink to='/change-password' className='nav-link'>Change Password</NavLink>
+//     <NavLink to='/sign-out' className='nav-link'>Sign Out</NavLink>
+//   </Fragment>
+// )
+//
+// const unauthenticatedOptions = (
+//   <Fragment>
+//     <NavLink to='/sign-up' className='nav-link'>Sign Up</NavLink>
+//     <NavLink to='/sign-in' className='nav-link'>Sign In</NavLink>
+//   </Fragment>
+// )
 
 const alwaysOptions = (
   <Fragment>
-    <NavLink exact to='/' className='nav-link'>Home</NavLink>
+    <NavLink exact to='/' className='nav-link'>Events</NavLink>
+    <NavLink exact to='/about' className='nav-link'>About</NavLink>
+    <NavLink exact to='/community' className='nav-link'>Community</NavLink>
   </Fragment>
 )
 
 const Header = ({ user }) => (
   <Navbar bg='primary' variant='dark' expand='md'>
     <Navbar.Brand>
-      <Link to='/' style={{ color: '#FFF', textDecoration: 'none' }}>react-auth-template</Link>
+      <a className="logo_image">
+        <img src="tottenham_hotspurs_cockerel_white_logo.svg" width="30" height="30" background="white" alt=""/>
+      </a>
+      <h className="logo">
+      Austin Spurs Supporters Group</h>
     </Navbar.Brand>
     <Navbar.Toggle aria-controls='basic-navbar-nav' />
     <Navbar.Collapse id='basic-navbar-nav'>
@@ -35,7 +41,7 @@ const Header = ({ user }) => (
           <span className='navbar-text mr-2'>Welcome, {user.email}</span>
         )}
         {alwaysOptions}
-        {user ? authenticatedOptions : unauthenticatedOptions}
+        {/*   {user ? authenticatedOptions : unauthenticatedOptions} */}
       </Nav>
     </Navbar.Collapse>
   </Navbar>
